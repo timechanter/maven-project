20ddaf5262f654c0ec0a3d236584adbba83ef12a
Justin Smith <jjsmith@agiledigital.com.au>
2018-10-09 14:59:00 +1100

    Initial commit

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,35 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.print("\n\nJunit Test Case #1: HTTP Dynamic Port: " + dynamicPort.getNumber() + "\n\n");
+		runFlowAndExpect("mavenFlow", "Hello Maven");
+	}
+
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("flightsFlow");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json", contentType);
+	}
+
+	@Override
+	protected String[] getConfigFiles() {
+		String[] files = { "maven-project.xml", "config.xml" };
+		return files;
+	}
+
+}
